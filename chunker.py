f844ebbf2db785c48503c6993e7a585e208a5e28
SAM12302 <sameer.younas83@gmail.com>
2026-06-13 15:51:36 +0500

    Finally done with chunker.py

--- chunker.py
+++ chunker.py
@@ -1,9 +1,6 @@
 import json
 import re
 
-with open("output.json", 'r') as file:
-    all_dict = json.load(file)
-
 def clean(text):
     """Remove noise from the text for RAG pipeline.
 
@@ -21,9 +18,48 @@ def clean(text):
     final_text = re.sub(r"\n{3,}", "\n\n", final_text)
     return final_text
 
+def chunk(text):
+    all_chunks = []
+    current_lines = []
+    current_section = ""
+
+    for line in text.split("\n"):
+        heading = re.match(r"^(#{1,6})\s+(.*)", line)
+        if heading:
+            heading = heading.group(2)
+            # print("Heading\n", heading)
+
+            if current_lines:
+                all_chunks.append({
+                    "current_section": current_section,
+                    "current_lines": "\n".join(current_lines).strip(),
+                })
+            current_section = heading
+            current_lines = []
+        else:
+            # If it is a normal line, add it to our text collection
+            if line.strip():  # Skips empty blank lines
+                current_lines.append(line)
+
+
+    # don't forget the last chunk after loop ends
+    all_chunks.append({
+        "current_section": current_section,
+        "current_lines": "\n".join(current_lines).strip(),
+    })
+    return all_chunks
+
+
+with open("output.json", 'r') as file:
+    print("Reading from output.json file\n")
+    all_dict = json.load(file)
+
 for i in range(len(all_dict)):
     all_dict[i]["clean_text"] = clean(all_dict[i]["raw_text"])
+    all_dict[i]["chunks"] = chunk(all_dict[i]["clean_text"])
 
-with open("output.json", "w") as file:
-    print(all_dict[0]["clean_text"])
+# all_dict[0]["chunks"] = chunk(all_dict[0]["clean_text"])
+
+with open("chunks.json", "w") as file:
+    print("Writing to chunks.json file\n")
     json.dump(all_dict, file)
