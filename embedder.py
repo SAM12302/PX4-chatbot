74d278ecd60444f38f5d6de25f7c3a186603793e
SAM12302 <sameer.younas83@gmail.com>
2026-06-14 16:43:10 +0500

    Done with embedder.py file for embedding creation

---
+++ embedder.py
@@ -0,0 +1,21 @@
+import dotenv
+from retry import retry
+import os
+from huggingface_hub import InferenceClient
+
+dotenv.load_dotenv(".env")
+
+model_id = os.getenv("model_id")
+hf_token = os.getenv("hf_token")
+
+inference_client = InferenceClient(
+    api_key=hf_token, provider="hf-inference"
+)
+
+@retry(tries=3, delay=2)
+def query(text):
+    result = inference_client.feature_extraction(
+        text,
+        model=model_id
+    )
+    return result
